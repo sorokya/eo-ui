09868038176232af957baceee8206b63984f4758
Richard Leek <richard@richardleek.com>
2025-11-20 01:38:31 -0500

    Fucking piece of shit is working in storybook and in the build

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,4 +1,3 @@
-import path from 'node:path';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
@@ -25,7 +24,6 @@ export default {
             url: 'copy', // copy the file
             assetsPath: 'assets', // folder inside dist/
             useHash: true, // optional: hashed filenames
-            basePath: path.resolve('src', 'assets'),
           },
         ]),
       ],

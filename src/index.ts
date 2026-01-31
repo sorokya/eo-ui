a746fae9d9d6d4cb2010ae13dcda974426a01650
Richard Leek <richard@richardleek.com>
2026-01-31 11:39:53 +0100

    Fix package exports

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ import './fonts.css';
 import './variables.css';
 
 export * from './Button/button';
+export * from './Checkbox/checkbox';
+export * from './Label/label';
+export * from './Panel/panel';
+export * from './TextBox/text-box';
